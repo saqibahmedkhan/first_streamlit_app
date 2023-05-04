6f162e9d339f09eb74f931f45805293383567918
saqibahmedkhan <129554182+saqibahmedkhan@users.noreply.github.com>
2023-05-04 15:36:28 +0500

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -3,8 +3,8 @@ import streamlit as st
 
 st.title("My First Streamlit App")
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
 st.text("🥣 🥗 🐔 🥑🍞")
