4f407a64bac8a52d1477aeb356386aa6f3ea4507
saqibahmedkhan <129554182+saqibahmedkhan@users.noreply.github.com>
2023-05-04 15:43:50 +0500

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -1,10 +1,10 @@
-
 import streamlit as st
-
+import pandas as pd
 st.title("My First Streamlit App")
-
 st.header('Breakfast Menu')
 st.text('Omega 3 & Blueberry Oatmeal')
 st.text('Kale, Spinach & Rocket Smoothie')
 st.text('Hard-Boiled Free-Range Egg')
 st.text("🥣 🥗 🐔 🥑🍞")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
