96a46760dad75931b05d1a8518e50e85907bf006
saqibahmedkhan <129554182+saqibahmedkhan@users.noreply.github.com>
2023-12-26 19:08:52 +0500

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -1,27 +1,86 @@
 import streamlit as st
-import pandas as pd
-st.title("My First Streamlit App")
-st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
-st.text("🥣 🥗 🐔 🥑🍞")
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-st.dataframe(my_fruit_list)
-st.multiselect("Pick some fruits:", list(my_fruit_list.index))
-my_fruit_list = my_fruit_list.set_index('Fruit')
-import requests as rq
-fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
-st.header("Fruityvice Fruit Advice!")
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-st.dataframe(fruityvice_normalized)
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
-import showflake.connector
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-st.text("Hello from Snowflake:")
-st.text(my_data_row)
+import pandas as pd 
+import plotly.express as px
+
+
+
+uploaded_file=st.file_uploader('Choose a CSV file containing only numerical values', 
+                 type='CSV', help='The file should contain only one column with no header', on_change=None)
+
+
+
+if uploaded_file is not None:
+    dfExpModel = pd.read_csv(uploaded_file,header=None, names=['Temp'])
+
+
+alpha=st.slider(
+    'Select value for Alpha',0.0,1.0,0.2,
+)
+
+st.write('')
+st.write('')
+
+
+
+beta=st.slider(
+    'Select value for Beta',0.0,1.0,0.3,
+)
+
+st.write('')
+st.write('')
+
+
+
+
+dfExpModel.insert(0, 'DayNumber', range(1, len(dfExpModel) + 1))
+
+dfHoltsMethod=dfExpModel.copy()
+
+dfExpModel['S(t)'] = dfExpModel['Temp'].copy()
+for i in range(1, len(dfExpModel)):
+    dfExpModel.at[i, 'S(t)'] = alpha * dfExpModel.at[i, 'Temp'] + (1-(alpha)) * dfExpModel.at[i - 1, 'S(t)']
+
+
+
+dfHoltsMethod['S(t)']=dfHoltsMethod['Temp'].copy()
+dfHoltsMethod['T(t)']=dfHoltsMethod['Temp'].copy()
+dfHoltsMethod['F(t)']=dfHoltsMethod['Temp'].copy()
+
+for i in range(0, len(dfHoltsMethod)):
+    if i==0:
+        dfHoltsMethod.at[i, 'S(t)'] = alpha * dfHoltsMethod.at[i, 'Temp'] 
+        dfHoltsMethod.at[i, 'T(t)'] = beta * dfHoltsMethod.at[i, 'S(t)'] 
+        dfHoltsMethod.at[i, 'F(t)']= None
+    else:
+        dfHoltsMethod.at[i, 'S(t)'] = (alpha * dfHoltsMethod.at[i, 'Temp']) + ((1-(alpha)) * (dfHoltsMethod.at[i - 1, 'S(t)']+dfHoltsMethod.at[i - 1, 'T(t)']))
+        dfHoltsMethod.at[i, 'T(t)'] = beta * ((dfHoltsMethod.at[i, 'S(t)'] - dfHoltsMethod.at[i-1, 'S(t)'])) + ((1-beta) * dfHoltsMethod.at[i - 1, 'T(t)']) 
+        dfHoltsMethod.at[i, 'F(t)']=  dfHoltsMethod.at[i-1, 'S(t)'] + dfHoltsMethod.at[i-1, 'T(t)']
+
+
+
+
+
+
+comparison_values = pd.concat([dfExpModel[['Temp','S(t)']], dfHoltsMethod['F(t)']], axis=1)
+
+comparison_values = comparison_values.rename(columns={'Temp': 'Actual', 'S(t)': 'ExpResult', 'F(t)': 'HoltResult'})
+
+comparison_values = comparison_values.drop(comparison_values.index[0]).reset_index(drop=True)
+
+
+
+
+Comparison_fig = px.line(comparison_values[['Actual','ExpResult','HoltResult']], 
+                title='Actual vs ExpResult vs HoltResult')
+st.plotly_chart(Comparison_fig, use_container_width=True)
+
+
+
+st.write('')
+st.write('Data of first 10 records when using Exponential smoothing model:')
+st.dataframe(dfExpModel.head(10))
+
+st.write('Data of first 10 records whne using Holt''s Method:')
+st.dataframe(dfHoltsMethod.head(10))
+
+
